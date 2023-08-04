20ec5335fe20311580dc89104a67aec94d9e2688
austinsamuel <austinsamuel733@gmail.com>
2023-08-04 22:40:41 +0100

    Part 2 - Using dynamic notes

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,21 @@ import Header from './Header';
 import './App.css';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
 
 function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //   <Header />
+  // console.log(notes);
 
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+
+  return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return <Note
+          key={note.key}
+          title={note.title}
+          content={note.content} />
+      })}
       <Footer />
     </div>
 
